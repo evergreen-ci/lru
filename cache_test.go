ca94ad615cfc1d1109d6a2ba5ac405e5b70af393
tycho garen <garen@tychoish.com>
2017-03-18 03:35:25 -0400

    update imports

--- cache_test.go
+++ cache_test.go
@@ -8,10 +8,10 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/mongodb/grip"
 	"github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	"github.com/tychoish/grip"
 )
 
 type CacheSuite struct {
